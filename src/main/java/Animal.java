1941d6217bacaa0bafb33fdf58b0f405ba3d5272
Janek Brandt <brandt.janek@gmail.com>
2017-04-07 10:16:23 -0700

    add enum for health and age, method to add details and getters for health, age and identifier

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,87 +3,118 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Animal {
-  public String name;
-  public int id;
-
-  public Animal(String name) {
-    this.name = name;
-    this.id = id;
+  public enum Health {
+    ILL,
+    OKAY,
+    HEALTHY;
+  }
+  public enum Age {
+    NEWBORN,
+    YOUNG,
+    ADULT;
   }
 
-  public String getName() {
-    return name;
+  private int id;
+  private int speciesId;
+  private Health health;
+  private Age age;
+  private String identifier;
+
+  public Animal(int speciesId) {
+    this.speciesId = speciesId;
+  }
+
+  public int getSpeciesId() {
+    return this.speciesId;
   }
 
   public int getId() {
-    return id;
+    return this.id;
   }
 
-  @Override
-  public boolean equals(Object otherAnimal) {
-    if(!(otherAnimal instanceof Animal)) {
-      return false;
-    } else {
-      Animal newAnimal = (Animal) otherAnimal;
-      return this.getName().equals(newAnimal.getName());
-    }
+  public void addDetail(Health health, Age age, String identifier) {
+    this.health = health;
+    this.age = age;
+    this.identifier = identifier;
   }
 
-  public void save() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO animals (name) VALUES (:name);";
-      this.id = (int) con.createQuery(sql, true)
-        .addParameter("name", this.name)
-        .executeUpdate()
-        .getKey();
-    }
+  public String getHealth() {
+    return this.health.toString();
   }
 
-  public static List<Animal> all() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM animals;";
-      return con.createQuery(sql)
-        .executeAndFetch(Animal.class);
-    }
+  public String getAge() {
+    return this.age.toString();
   }
 
-  public static Animal find(int id) {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM animals WHERE id=:id;";
-      Animal animal = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Animal.class);
-      return animal;
-    }
+  public String getIdentifier() {
+    return this.identifier;
   }
 
-  public void updateName(String name) {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "UPDATE animals SET name=:name WHERE id=:id;";
-      con.createQuery(sql)
-        .addParameter("id", id)
-        .addParameter("name", name)
-        .executeUpdate();
-    }
-  }
-
-  public void delete() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM animals WHERE id=:id;";
-      con.createQuery(sql)
-        .addParameter("id", id)
-        .executeUpdate();
-    }
-  }
-
-  public List<Sighting> getSightings() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM sightings WHERE animal_id=:id;";
-        List<Sighting> sightings = con.createQuery(sql)
-          .addParameter("id", id)
-          .executeAndFetch(Sighting.class);
-      return sightings;
-    }
-  }
+  // @Override
+  // public boolean equals(Object otherAnimal) {
+  //   if(!(otherAnimal instanceof Animal)) {
+  //     return false;
+  //   } else {
+  //     Animal newAnimal = (Animal) otherAnimal;
+  //     return this.getName().equals(newAnimal.getName());
+  //   }
+  // }
+  //
+  // public void save() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "INSERT INTO animals (name) VALUES (:name);";
+  //     this.id = (int) con.createQuery(sql, true)
+  //       .addParameter("name", this.name)
+  //       .executeUpdate()
+  //       .getKey();
+  //   }
+  // }
+  //
+  // public static List<Animal> all() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM animals;";
+  //     return con.createQuery(sql)
+  //       .executeAndFetch(Animal.class);
+  //   }
+  // }
+  //
+  // public static Animal find(int id) {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM animals WHERE id=:id;";
+  //     Animal animal = con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeAndFetchFirst(Animal.class);
+  //     return animal;
+  //   }
+  // }
+  //
+  // public void updateName(String name) {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "UPDATE animals SET name=:name WHERE id=:id;";
+  //     con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .addParameter("name", name)
+  //       .executeUpdate();
+  //   }
+  // }
+  //
+  // public void delete() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "DELETE FROM animals WHERE id=:id;";
+  //     con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeUpdate();
+  //   }
+  // }
+  //
+  // public List<Sighting> getSightings() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM sightings WHERE animal_id=:id;";
+  //       List<Sighting> sightings = con.createQuery(sql)
+  //         .addParameter("id", id)
+  //         .executeAndFetch(Sighting.class);
+  //     return sightings;
+  //   }
+  // }
 
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -11,68 +11,92 @@ public class AnimalTest {
 
   @Test
   public void animal_instantiatesCorrectly_false() {
-    Animal testAnimal = new Animal("Deer");
+    Animal testAnimal = new Animal(1);
     assertEquals(true, testAnimal instanceof Animal);
   }
 
   @Test
-  public void getName_animalInstantiatesWithName_Deer() {
-    Animal testAnimal = new Animal("Deer");
-    assertEquals("Deer", testAnimal.getName());
+  public void getSpeciesId_returnsSpeciesIdOfAnimal() {
+    Animal testAnimal = new Animal(1);
+    assertEquals(1, testAnimal.getSpeciesId());
   }
 
   @Test
-  public void equals_returnsTrueIfNameIsTheSame_false() {
-    Animal firstAnimal = new Animal("Deer");
-    Animal anotherAnimal = new Animal("Deer");
-    assertTrue(firstAnimal.equals(anotherAnimal));
+  public void addDetail_addMoreInformationToAnimal() {
+    Animal testAnimal = new Animal(1);
+    testAnimal.addDetail(Animal.Health.OKAY, Animal.Age.ADULT, "Tag on ear");
+    assertEquals("OKAY", testAnimal.getHealth());
+    assertEquals("ADULT", testAnimal.getAge());
+    assertEquals("Tag on ear", testAnimal.getIdentifier());
   }
 
   @Test
-  public void save_assignsIdToObjectAndSavesObjectToDatabase() {
-    Animal testAnimal = new Animal("Deer");
-    testAnimal.save();
-    Animal savedAnimal = Animal.all().get(0);
-    assertEquals(testAnimal.getId(), savedAnimal.getId());
+  public void enumHealth_returnPossibleHealthValues() {
+    Animal testAnimal = new Animal(1);
+    for (Animal.Health health: Animal.Health.values()) {
+      System.out.println(health);
+    }
+    // System.out.println(Animal.Health.values());
   }
 
-  @Test
-  public void all_returnsAllInstancesOfAnimal_false() {
-    Animal firstAnimal = new Animal("Deer");
-    firstAnimal.save();
-    Animal secondAnimal = new Animal("Black Bear");
-    secondAnimal.save();
-    assertEquals(true, Animal.all().get(0).equals(firstAnimal));
-    assertEquals(true, Animal.all().get(1).equals(secondAnimal));
-  }
-
-  @Test
-  public void find_returnsAnimalWithSameId_secondAnimal() {
-    Animal firstAnimal = new Animal("Deer");
-    firstAnimal.save();
-    Animal secondAnimal = new Animal("Black Bear");
-    secondAnimal.save();
-    assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
-  }
-
-  @Test
-  public void delete_deletesAnimalFromDatabase_0() {
-    Animal testAnimal = new Animal("Deer");
-    testAnimal.save();
-    testAnimal.delete();
-    assertEquals(0, Animal.all().size());
-  }
-
-  public void updateName_updatesAnimalNameInDatabase_String() {
-    Animal testAnimal = new Animal("Deer");
-    testAnimal.save();
-    testAnimal.updateName("Buck");
-    assertEquals("Buck", testAnimal.getName());
-  }
-
-  @Test
-  public void find_returnsNullWhenNoAnimalFound_null() {
-    assertTrue(Animal.find(999) == null);
-  }
+  // @Test
+  // public void getName_animalInstantiatesWithName_Deer() {
+  //   Animal testAnimal = new Animal(1);
+  //   assertEquals(1, testAnimal.getName());
+  // }
+  //
+  // @Test
+  // public void equals_returnsTrueIfNameIsTheSame_false() {
+  //   Animal firstAnimal = new Animal(1);
+  //   Animal anotherAnimal = new Animal(1);
+  //   assertTrue(firstAnimal.equals(anotherAnimal));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObjectAndSavesObjectToDatabase() {
+  //   Animal testAnimal = new Animal(1);
+  //   testAnimal.save();
+  //   Animal savedAnimal = Animal.all().get(0);
+  //   assertEquals(testAnimal.getId(), savedAnimal.getId());
+  // }
+  //
+  // @Test
+  // public void all_returnsAllInstancesOfAnimal_false() {
+  //   Animal firstAnimal = new Animal(1);
+  //   firstAnimal.save();
+  //   Animal secondAnimal = new Animal(2);
+  //   secondAnimal.save();
+  //   assertEquals(true, Animal.all().get(0).equals(firstAnimal));
+  //   assertEquals(true, Animal.all().get(1).equals(secondAnimal));
+  // }
+  //
+  // @Test
+  // public void find_returnsAnimalWithSameId_secondAnimal() {
+  //   Animal firstAnimal = new Animal(1);
+  //   firstAnimal.save();
+  //   Animal secondAnimal = new Animal(2);
+  //   secondAnimal.save();
+  //   assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
+  // }
+  //
+  // @Test
+  // public void delete_deletesAnimalFromDatabase_0() {
+  //   Animal testAnimal = new Animal(1);
+  //   testAnimal.save();
+  //   testAnimal.delete();
+  //   assertEquals(0, Animal.all().size());
+  // }
+  //
+  // public void updateName_updatesAnimalNameInDatabase_String() {
+  //   Animal testAnimal = new Animal(1);
+  //   testAnimal.save();
+  //   testAnimal.updateName(2);
+  //   assertEquals(2, testAnimal.getName());
+  // }
+  //
+  // @Test
+  // public void find_returnsNullWhenNoAnimalFound_null() {
+  //   assertTrue(Animal.find(999) == null);
+  // }
 
 }
