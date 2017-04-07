3cc0b392f809f34d1fd0480cb2eb311fd27be885
Janek Brandt <brandt.janek@gmail.com>
2017-04-07 11:34:07 -0700

    add save and all methods to sightings

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -44,35 +44,33 @@ public class Sighting {
       return false;
     } else {
       Sighting newSighting = (Sighting) otherSighting;
-      return this.getSpeciesId() == (newSighting.getSpeciesId()) &&
+      return this.getSpeciesId() == newSighting.getSpeciesId() &&
              this.getLocation().equals(newSighting.getLocation()) &&
-             this.getTime() == newSighting.getTime() &&
+            //  this.getTime() == newSighting.getTime() &&
              this.getUserId() == newSighting.getUserId() &&
              this.getId() == newSighting.getId();
     }
   }
 
-  // public void save() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "INSERT INTO sightings (animal_id, location, ranger_name) VALUES (:animal_id, :location, :ranger_name);";
-  //     this.id = (int) con.createQuery(sql, true)
-  //       .addParameter("animal_id", this.animal_id)
-  //       .addParameter("location", this.location)
-  //       .addParameter("ranger_name", this.ranger_name)
-  //       .throwOnMappingFailure(false)
-  //       .executeUpdate()
-  //       .getKey();
-  //   }
-  // }
-  //
-  // public static List<Sighting> all() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "SELECT * FROM sightings;";
-  //     return con.createQuery(sql)
-  //       .throwOnMappingFailure(false)
-  //       .executeAndFetch(Sighting.class);
-  //   }
-  // }
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO sightings (speciesId, location, userId, time) VALUES (:speciesId, :location, :userId, now());";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("speciesId", this.speciesId)
+        .addParameter("location", this.location)
+        .addParameter("userId", this.userId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Sighting> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sightings;";
+      return con.createQuery(sql)
+        .executeAndFetch(Sighting.class);
+    }
+  }
   //
   // public static Sighting find(int id) {
   //   try(Connection con = DB.sql2o.open()) {
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -53,29 +53,29 @@ public class SightingTest {
     assertTrue(testSighting.equals(anotherSighting));
   }
 
-  // @Test
-  // public void save_insertsObjectIntoDatabase_Sighting() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", "Ranger Avery");
-  //   testSighting.save();
-  //   assertEquals(true, Sighting.all().get(0).equals(testSighting));
-  // }
-  //
-  // @Test
-  // public void all_returnsAllInstancesOfSighting_true() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", "Ranger Avery");
-  //   testSighting.save();
-  //   Animal secondTestAnimal = new Animal("Badger");
-  //   secondTestAnimal.save();
-  //   Sighting secondTestSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", "Ranger Reese");
-  //   secondTestSighting.save();
-  //   assertEquals(true, Sighting.all().get(0).equals(testSighting));
-  //   assertEquals(true, Sighting.all().get(1).equals(secondTestSighting));
-  // }
-  //
+  @Test
+  public void save_insertsObjectIntoDatabase_Sighting() {
+    Species testSpecies = new Species("Deer", "Mammal", "Forest", false);
+    testSpecies.save();
+    Sighting testSighting = new Sighting(testSpecies.getId(), "45.472428, -121.946466", 1);
+    testSighting.save();
+    assertEquals(true, Sighting.all().get(0).equals(testSighting));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfSighting_true() {
+    Species testSpecies = new Species("Deer", "Mammal", "Forest", false);
+    testSpecies.save();
+    Sighting testSighting = new Sighting(testSpecies.getId(), "45.472428, -121.946466", 1);
+    testSighting.save();
+    Species secondSpecies = new Species("Black Bear", "Mammal", "Forest", false);
+    secondSpecies.save();
+    Sighting secondTestSighting = new Sighting (secondSpecies.getId(), "45.472428, -121.946466", 1);
+    secondTestSighting.save();
+    assertEquals(true, Sighting.all().get(0).equals(testSighting));
+    assertEquals(true, Sighting.all().get(1).equals(secondTestSighting));
+  }
+
   // @Test
   // public void find_returnsSightingWithSameId_secondSighting() {
   //   Animal testAnimal = new Animal("Deer");
